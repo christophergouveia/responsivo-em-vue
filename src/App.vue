d49a33988f49bbcb73961c94dabea0588162db11
Christopher Gouveia <christopherlaislara@gmail.com>
2024-07-09 11:01:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+document.title = 'Aula de responsividade'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Página Inicial</RouterLink>
+        <RouterLink to="/about"></RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,79 @@
+<template>
+  <main>
+    <h1>Bolsonaro e Astolfo</h1>
+    <h2>Turma da pesada</h2>
+    <h2></h2>
+    <div class="container">
+      <img
+        class="imagemUnica"
+        src="https://pm1.aminoapps.com/7038/4618475f0aaefb50863a3e48ed37c3d7cf099a6ar1-1004-1022v2_uhq.jpg"
+      />
+    </div>
+    <div class="container">
+      <div class="sub-container">
+        <img
+          class="imagem"
+          src="https://img.elo7.com.br/product/zoom/45A714A/caneca-meme-bolsotse-bolsonaro-comunista-meme.jpg"
+        />
+      </div>
+      <div class="sub-container">
+        <img
+          class="imagem"
+          src="https://uploads.emaisgoias.com.br/2021/08/c17f8993-whatsapp-image-2021-08-28-at-12.13.06.jpeg"
+        />
+      </div>
+    </div>
+    <iframe
+      src="https://embedplayer.online/video/68d14581775852c113997d94803a0855"
+      width="50%"
+      height="600px"
+    ></iframe>
+    <table>
+      <tr>
+        <th>Item</th>
+        <th>Nome do produto</th>
+        <th>Imagem</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Carro Mercedes-Benz C180</td>
+        <td>
+          <img
+            width="200px"
+            src="https://autoentusiastas.com.br/ae/wp-content/uploads/2016/08/DSC03297.jpg"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Avião Boeing 737</td>
+        <td>
+          <img
+            width="200px"
+            src="https://www.melhoresdestinos.com.br/wp-content/uploads/2021/06/boeing-737-max-10-capa-2021-2.jpg"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Moto Honda CB 1000R</td>
+        <td>
+          <img
+            width="200px"
+            src="https://cdn.autopapo.com.br/box/uploads/2023/09/18142046/link-7-1-1920x1280.jpg"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Bicicleta Santa Cruz Heckler</td>
+        <td>
+          <img
+            width="200px"
+            src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSTOJ-OBL2da9cag8xsicORc-3BeIvy9l0zm1CE2VceN1evmJ7dpt5_pOSNGDLxnLTYke0HSondF8PzrBsx2EmqCZYi4Kwu&usqp=CAY"
+          />
+        </td>
+      </tr>
+    </table>
+  </main>
+</template>
