400033913e24ec222777ebff8a41774ca6117240
Christopher Gouveia <christopherlaislara@gmail.com>
2024-08-02 11:10:41 -0300

    Update da atividade

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
+import VideosPage from '../views/VideosView.vue'
+import ImagensView from '@/views/ImagensView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage
+    },
+    {
+      path: '/videos',
+      name: 'videos',
+      component: () => VideosPage
+    },
+    {
+      path: '/imagens',
+      name: 'imagens',
+      component: () => ImagensView
+    },
+    {
+      path: '/tabela',
+      name: 'tabela',
+      component: () => import('../views/TabelaView.vue')
     }
   ]
 })
---
+++ src/views/TabelaView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import TabelaComponent from '../components/TabelaComponent.vue'
+</script>
+
+<template>
+  <h1>Componente - Tabela</h1>
+  <TabelaComponent />
+</template>
